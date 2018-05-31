31d721de196439b8a98754b05d9746044d1d19a6
marthamareal <marthamareal@gmail.com>
2018-05-31 16:52:12 +0300

    UI for signup and login

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import pymysql
+
+# MySQL Configurations
+
+app = Flask(__name__)
+db = pymysql.connect("localhost", "root", "F0226@vak", "OrderMeal")
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run()
